55f1407df07e8659161b0e62836571dbdc8c5929
opiegraham33 <66490738+opiegraham33@users.noreply.github.com>
2020-06-05 15:11:37 -0400

    Add files via upload

---
+++ myFunctions.js
@@ -0,0 +1,19 @@
+/* Graham Opie
+   3/31/2019
+   My Function List
+*/
+
+//Burpees video window
+function burpees() {
+	window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+//Plank video window
+function plank() {
+	window.open("scripts/plank.html", "_blank", "width=610, height=360");
+}
+
+//Mountain climber video window
+function mtnClimber() {
+	window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+}
